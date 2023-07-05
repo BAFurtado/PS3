7e2b889fc8553b636647144f1816e2cb29f5e5db
bafurtado <furtadobb@gmail.com>
2023-07-05 14:28:30 -0300

    fixed parameter name

--- markets/goods.py
+++ markets/goods.py
@@ -1,6 +1,6 @@
 
 def consume(sim):
     firms = list(sim.consumer_firms.values())
-    origin = sim.PARAMS['TAX_ON_DESTIN']
+    origin = sim.PARAMS['TAX_ON_ORIGIN']
     for family in sim.families.values():
         family.consume(firms, sim.central, sim.regions, sim.PARAMS, sim.seed, sim.clock.year, sim.clock.months, origin)
