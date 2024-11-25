fcb9c7f9a1f39f7e3bdb0903d08a950c40b4952f
bafurtado <furtadobb@gmail.com>
2024-11-25 14:43:31 -0300

    Read data for spatial gdp from IBGE

---
+++ analysis/validation/read_pib.py
@@ -0,0 +1,29 @@
+import pandas as pd
+
+
+# Defina as larguras das colunas com base nas posições
+colunas = [
+    (0, 4),    # Ano
+    (46, 53),  # Código do município
+    (95, 95 + 85), # Código RM
+    (412, 419), # Código concentração urbana
+    (896, 915),  # valor adicionado a preços correntes
+    (934, 953) # pib a preços correntes
+    ]
+
+# Nomes das colunas
+nomes_colunas = [
+    "ano",
+    "cod_mun",
+    "rm",
+    "cod_conc_urb",
+    "va_corrente",
+    "pib_corrente"
+]
+
+
+if __name__ == '__main__':
+    f = 'pib_municipios_2009_2021_original.txt'
+    d = pd.read_fwf(f, colspecs=colunas, names=nomes_colunas, encoding='latin-1')
+    d = d[d.ano == 2021]
+    d.to_csv('pib_municipios2021.csv', index=False)
