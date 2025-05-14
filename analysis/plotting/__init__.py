2abea75f59e6ed0ff47c1b0ee6152b9a18a5776d
bafurtado <furtadobb@gmail.com>
2025-05-14 15:20:06 -0300

    fix plot_firms, families, houses

--- analysis/plotting/__init__.py
+++ analysis/plotting/__init__.py
@@ -217,7 +217,8 @@ class Plotter:
                 'name': 'time_on_market'
             }
         }
-        df = dat.groupby(['month', 'mun_id'], as_index=False).mean()
+        numeric_cols = dat.select_dtypes(include='number').columns
+        df = dat.groupby(['month', 'mun_id'], as_index=False)[numeric_cols].mean()
         for k, d in to_plot.items():
             title = d['title']
             name = d['name']
@@ -245,7 +246,8 @@ class Plotter:
             }
         }
 
-        df = dat.groupby(['month', 'mun_id'], as_index=False).mean()
+        numeric_cols = dat.select_dtypes(include='number').columns
+        df = dat.groupby(['month', 'mun_id'], as_index=False)[numeric_cols].mean()
         for k, d in to_plot.items():
             title = d['title']
             name = d['name']
@@ -337,7 +339,8 @@ class Plotter:
             self.save_fig(fig, '{}'.format(col))
 
         title = 'Median of number of employees by firm, by month'
-        firms_stats = dat.groupby(['month', 'firm_id'], as_index=False).median()
+        numeric_cols = dat.select_dtypes(include='number').columns
+        firms_stats = dat.groupby(['month', 'firm_id'], as_index=False)[numeric_cols].median()
         dat_to_plot = firms_stats.pivot(index='month', columns='firm_id', values='number_employees').astype(float)
         dats_to_plot = [dat_to_plot[c] for c in dat_to_plot.columns.values]
         labels = ['Firm {}'.format(i) for i, _ in enumerate(dat_to_plot.columns.values)]
